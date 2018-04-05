a248958fb95153e98fb7b95bf885840546df4697
bigfa <jigoulee@gmail.com>
2018-04-05 15:52:02 +0800

    add bitcron branch

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="site-footer">
-    <div class="layoutSingleColumn--wide">
-        just a <a href="https://fatesinger.com" target="_blank">bigfa</a> wordpress theme.
-    </div>
-</footer>
-<?php wp_footer();?>
-</body>
-</html>
--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('COFFIN_VERSION', '1.0.0');
-define('COFFIN_DEBUG', false);
-
-require('inc/setup.php');
-require('inc/static.php');
-require('inc/base.php');
